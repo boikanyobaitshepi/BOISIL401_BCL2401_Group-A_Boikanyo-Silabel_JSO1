4c6a2914b9ffc671569677c47b22b72eb19f254e
boikanyobaitshepi <156679487+boikanyobaitshepi@users.noreply.github.com>
2024-03-12 15:40:23 +0200

    commit

--- Module_1_StudentNo_Classcode_Group_Name-Surname_JSL01-main/Module_1_StudentNo_Classcode_Group_Name-Surname_JSL01-main/script.js
+++ Module_1_StudentNo_Classcode_Group_Name-Surname_JSL01-main/Module_1_StudentNo_Classcode_Group_Name-Surname_JSL01-main/script.js
@@ -1,12 +1,18 @@
 function validateSyntax() {
-    let input = document.getElementById('petInput').value;
+    let input = document.getElementById('petInput').value.trim();
+    let regex = /^pet_\d+[a-zA -Z]+$/;
+    let isValid = regex.test(petinput);
+
     // Validation logic goes here
-    let result = ''; // Placeholder for validation result
 
-    // TODO: Write your validation logic here
-        // Check if input starts with 'pet_' and followed by alphanumeric characters
+    let resultElement= document.getElementById("result"); // Placeholder for validation result
 
-            document.getElementById('result').innerText = result;
+    if (isValid) {
+        resultElement.innerHTML = "Valid Syntax." + '<span style = "color: green; font-size: 60px;">&#x25cf;</span>';
+    }else{
+        resultElement.innerHTML = "Invalid Syntax." + '<span style = "color: red; font-size: 60px;">&#x25cf;</span>';
+    }
 }
+            
 
 
